f432a42a125e3fdfd3994566e3fc76829f60be32
llRaGGinGll <jack.ctl.998897961@gmail.com>
2023-03-18 02:50:42 +0700

    đầu tiên tôi đã kéo MainAcitivity vào package activities -tôi có làm code trong cái AndroidManifest.xml và làm trong cái acitvity_sign_up.xml -sau do làm code trên class SignUpActivity.java và làm code tạo cái dữ liệu trên file Cóntants với làm chức năng chính trên file PreferenceManager và sau do import file Preference vào file class chính signup để hoàn thanh chức năng đăng kí dược
    bây giờ tôi đã đăng kí được từ máy tôi và hiện lên máy bạn Kiattysack dâtbase firebase Chính trên browser của máy nó

--- app/src/main/java/com/teamone/chatboot/activities/MainActivity.java
+++ app/src/main/java/com/teamone/chatboot/activities/MainActivity.java
@@ -1,8 +1,10 @@
-package com.teamone.chatboot;
+package com.teamone.chatboot.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.teamone.chatboot.R;
 
 public class MainActivity extends AppCompatActivity {
 
